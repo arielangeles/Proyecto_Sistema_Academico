d925388e2ddf40d2ebc90daf62234e0dfb51a640
Hellrose09 <49009311+Hellrose09@users.noreply.github.com>
2019-07-05 20:02:25 -0400

    Add files via upload

---
+++ Registro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    public class Registro
+    {
+        public string subject { get; set; }
+        public string credValue { get; set; }
+        public short grade { get; set; }
+
+        public Registro(string Subject, string CredValue, short Grade)
+        {
+            subject = Subject;
+            credValue = CredValue;
+            grade = Grade;
+        }
+    }
+}
